0bec801ae6d2a213a4e0c8025b671fb3bf866d97
nick-wilmsmeyer <106624939+nick-wilmsmeyer@users.noreply.github.com>
2022-06-01 10:11:06 -0500

    Modal Changes
    Adding radio boxes and checkboxes to modal pages

--- index.php
+++ index.php
@@ -40,15 +40,220 @@
       </div>
       <!-- Add more modal-split divs to create more pages -->
       <div class="modal-body">
-        <div class="modal-split">1</div>
-		    <div class="modal-split">2</div>
-        <div class="modal-split">3</div>
-        <div class="modal-split">4</div>
-        <div class="modal-split">5</div>
-        <div class="modal-split">6</div>
-        <div class="modal-split">7</div>
-        <div class="modal-split">8</div>
-        <div class="modal-split">9</div>
+        <!--Modal Page 1-->
+        <div class="modal-split">
+          <h2 class="fw-light">How many guests will be eating?</h2>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+            <label class="form-check-label" for="exampleRadios1">1-5 Guests</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+            <label class="form-check-label" for="exampleRadios2">6-10 Guests</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3" checked>
+            <label class="form-check-label" for="exampleRadios3">11-20 Guests</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios4" value="option4">
+            <label class="form-check-label" for="exampleRadios4">21-50 Guests</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios5" value="option5">
+            <label class="form-check-label" for="exampleRadios5">51-99 Guests</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios6" value="option6">
+            <label class="form-check-label" for="exampleRadios6">100+ Guests</label>
+          </div>
+          <div class="progress modalProgressPadding">
+            <div class="progress-bar progress-bar-striped progress-bar-animated btns" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">20%</div>
+          </div>
+        </div>
+        <!--Modal Page 2-->
+		    <div class="modal-split">
+          <h2 class="fw-light">When will the meal be served?</h2>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+            <label class="form-check-label" for="exampleRadios1">Breakfast</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+            <label class="form-check-label" for="exampleRadios2">Mid-Morning Snack</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3">
+            <label class="form-check-label" for="exampleRadios3">Lunch</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios4" value="option4">
+            <label class="form-check-label" for="exampleRadios4">Afternoon Snack</label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios5" value="option5">
+            <label class="form-check-label" for="exampleRadios5">Dinner</label>
+          </div>
+          <div class="progress modalProgressPadding">
+            <div class="progress-bar progress-bar-striped progress-bar-animated btns" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">40%</div>
+          </div>
+        </div>
+        <!--Modal Page 3-->
+        <div class="modal-split">
+          <h2 class="fw-light">Which items will be included in the Catering Bundle?</h2>
+          <h4 class="fw-light">Check all that apply.</h2>
+          <div class="container">
+            <div class="row align-items-start">
+              <div class="col">
+                <ul class="list-unstyled">
+                  <li>
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                      <label class="form-check-label" for="defaultCheck1">Hot Drinks</label>
+                    </div>
+                  </li>
+                  <li>
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                      <label class="form-check-label" for="defaultCheck1">Cold Drinks</label>
+                    </div>
+                  </li>
+                  <li>
+                    <div class="form-check">
+                      <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                      <label class="form-check-label" for="defaultCheck1">Sweets & Deserts</label>
+                    </div>
+                  </li>
+                  <li>
+                    <label class="form-check-label fw-bold">Breakfast</label>
+                    <ul>
+                      <li>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                          <label class="form-check-label" for="defaultCheck1">Wraps</label>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                          <label class="form-check-label" for="defaultCheck1">Sandwiches</label>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                          <label class="form-check-label" for="defaultCheck1">Souffles</label>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                          <label class="form-check-label" for="defaultCheck1">Bakery & Pastries</label>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                          <label class="form-check-label" for="defaultCheck1">Bagels</label>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                          <label class="form-check-label" for="defaultCheck1">Parfaits</label>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                          <label class="form-check-label" for="defaultCheck1">Fruit Bowls</label>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                          <label class="form-check-label" for="defaultCheck1">Oatmeal</label>
+                        </div>
+                      </li>
+                    </li>
+                  </ul>
+              </div>
+              <div class="col">
+                <ul class="list-unstyled">
+                  <li>
+                    <label class="form-check-label fw-bold">Lunch & Dinner</label>
+                    <ul>
+                      <li>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                          <label class="form-check-label" for="defaultCheck1">Sandwiches</label>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                          <label class="form-check-label" for="defaultCheck1">Salads</label>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                          <label class="form-check-label" for="defaultCheck1">Soup</label>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                          <label class="form-check-label" for="defaultCheck1">Mac & Cheese</label>
+                        </div>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <label class="form-check-label fw-bold">Snacks</label>
+                    <ul>
+                      <li>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                          <label class="form-check-label" for="defaultCheck1">Bakery & Pastries</label>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                          <label class="form-check-label" for="defaultCheck1">Fruit Bowls</label>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                          <label class="form-check-label" for="defaultCheck1">Sandwiches</label>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                          <label class="form-check-label" for="defaultCheck1">Salads</label>
+                        </div>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          <div class="progress">
+            <div class="progress-bar progress-bar-striped progress-bar-animated btns" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">60%</div>
+          </div>
+        </div>
+        <!--Modal Page 4-->
+        <!--Modal Page 5-->
+        <div class="modal-split">
+          <h2 class="fw-light">Specific food requests</h2>
+          <h4 class="fw-light">Check all that apply.</h4>
+          <div class="progress">
+            <div class="progress-bar progress-bar-striped progress-bar-animated btns" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">100%</div>
+          </div>
+        </div>
       </div>
       <div class="modal-footer">
  <!--Nothing Goes Here but is needed! -->
